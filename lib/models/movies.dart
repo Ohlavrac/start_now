421fb090f32a712fcb74cafdb8e30e57f99c5c52
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-07-04 18:33:27 -0300

    adicionado filmes recomendados

---
+++ lib/models/movies.dart
@@ -0,0 +1,104 @@
+class Movies {
+  int? page;
+  List<Results>? results;
+  int? totalPages;
+  int? totalResults;
+
+  Movies({this.page, this.results, this.totalPages, this.totalResults});
+
+  Movies.fromJson(Map<String, dynamic> json) {
+    page = json['page'];
+    if (json['results'] != null) {
+      results = <Results>[];
+      json['results'].forEach((v) {
+        results!.add(new Results.fromJson(v));
+      });
+    }
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['page'] = this.page;
+    if (this.results != null) {
+      data['results'] = this.results!.map((v) => v.toJson()).toList();
+    }
+    data['total_pages'] = this.totalPages;
+    data['total_results'] = this.totalResults;
+    return data;
+  }
+}
+
+class Results {
+  bool? adult;
+  String? backdropPath;
+  List<int>? genreIds;
+  int? id;
+  String? mediaType;
+  String? title;
+  String? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  String? releaseDate;
+  bool? video;
+  double? voteAverage;
+  int? voteCount;
+
+  Results(
+      {this.adult,
+      this.backdropPath,
+      this.genreIds,
+      this.id,
+      this.mediaType,
+      this.title,
+      this.originalLanguage,
+      this.originalTitle,
+      this.overview,
+      this.popularity,
+      this.posterPath,
+      this.releaseDate,
+      this.video,
+      this.voteAverage,
+      this.voteCount});
+
+  Results.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    genreIds = json['genre_ids'].cast<int>();
+    id = json['id'];
+    mediaType = json['media_type'];
+    title = json['title'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'];
+    posterPath = json['poster_path'];
+    releaseDate = json['release_date'];
+    video = json['video'];
+    voteAverage = json['vote_average'];
+    voteCount = json['vote_count'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['adult'] = this.adult;
+    data['backdrop_path'] = this.backdropPath;
+    data['genre_ids'] = this.genreIds;
+    data['id'] = this.id;
+    data['media_type'] = this.mediaType;
+    data['title'] = this.title;
+    data['original_language'] = this.originalLanguage;
+    data['original_title'] = this.originalTitle;
+    data['overview'] = this.overview;
+    data['popularity'] = this.popularity;
+    data['poster_path'] = this.posterPath;
+    data['release_date'] = this.releaseDate;
+    data['video'] = this.video;
+    data['vote_average'] = this.voteAverage;
+    data['vote_count'] = this.voteCount;
+    return data;
+  }
+}
--- lib/repositories/repository_movie_detail.dart
+++ lib/repositories/repository_movie_detail.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:start_now/models/movies.dart';
 
 import '../models/movie_detail.dart';
 
 class RepositoryMovieDetail {
-  final String endpoint = "?api_key=816e85e9e115d47a54d5f262723dac3d&language=pt-Br 338953";
+  final String endpoint = "?api_key=816e85e9e115d47a54d5f262723dac3d&language=pt-Br";
+  final String endpointRecommendations = "/recommendations?api_key=816e85e9e115d47a54d5f262723dac3d&language=pt-BR&page=1";
 
   RepositoryMovieDetail();
 
@@ -16,4 +18,16 @@ class RepositoryMovieDetail {
       return null;
     }
   }
+
+  Future<List<Results>?> getRecommendations(String movieID) async {
+    Response response;
+    response = await Dio().get("https://api.themoviedb.org/3/movie/"+movieID+endpointRecommendations);
+    print(response);
+    if (response.statusCode == 200) {
+      
+      return Movies.fromJson(response.data).results;
+    } else {
+      return null;
+    }
+  } 
 }
