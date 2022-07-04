bd22191f91752cf0518e4ae5a8725b3194be53fe
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-07-04 07:15:34 -0300

    adicionado arquivos de texto e cor

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static final background = Color(0xFF111C2E);
+}
---
+++ lib/shared/themes/app_texts.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTexts {
+  static final overview = GoogleFonts.openSans(
+    fontSize: 14,
+    color: Colors.white,
+  );
+
+  static final title = GoogleFonts.openSans(
+    fontSize: 14,
+    color: Colors.white,
+    fontWeight: FontWeight.bold
+  );
+
+  static final tagname = GoogleFonts.openSans(
+    fontStyle: FontStyle.italic,
+    fontSize: 14,
+    color: Colors.white,
+    fontWeight: FontWeight.normal
+  );
+}
