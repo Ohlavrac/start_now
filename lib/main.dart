eb1744d636f8ba1d3a806482f0ae9803e7240c0d
Marcelo B <marcelobarros159@gmail.com>
2022-07-07 19:49:32 -0300

    alteração no main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/search_page.dart';
+import 'app_widget.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SearchPage(),
-    );
-  }
 }
